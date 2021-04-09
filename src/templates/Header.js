6eed5413378d3739b91bf066a0bbb24d8654cce1
Diego <d9730707@gmail.com>
2021-04-09 17:13:01 -0500

    cambio el link del titulo

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () =>{
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
-                    <a href="/">
+                    <a href="#/?page=1/">
                         MortyTienesKvenir
                     </a>
                 </h1>
